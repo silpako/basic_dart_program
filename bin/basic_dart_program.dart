c916d93f0f649a1316da27b8da8de0baea278818
silpako <silpako167@gmail.com>
2024-04-30 22:53:09 +0530

    first commit

---
+++ bin/basic_dart_program.dart
@@ -0,0 +1,5 @@
+import 'package:basic_dart_program/basic_dart_program.dart' as basic_dart_program;
+
+void main(List<String> arguments) {
+  print('Hello world: ${basic_dart_program.calculate()}!');
+}
---
+++ test/basic_dart_program_test.dart
@@ -0,0 +1,8 @@
+import 'package:basic_dart_program/basic_dart_program.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
